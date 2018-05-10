46824af66cf82e3bcc6e541f0c9f5301407cea9b
Jonathan Brown <jonathan@jonathanbrown.ca>
2018-05-10 16:38:20 -0400

    Added command for installing i3

---
+++ install_packages.sh
@@ -0,0 +1,10 @@
+apt-get update
+apt-get upgrade
+
+# install i3
+apt-get install libxcb1-dev libxcb-keysyms1-dev libpango1.0-dev libxcb-util0-dev libxcb-icccm4-dev libyajl-dev libstartup-notification0-dev libxcb-randr0-dev libev-dev libxcb-cursor-dev libxcb-xinerama0-dev libxcb-xkb-dev libxkbcommon-dev libxkbcommon-x11-dev autoconf libxcb-xrm-dev
+
+sudo add-apt-repository ppa:aguignard/ppa
+sudo apt-get update
+sudo apt-get install libxcb-xrm-dev
+
