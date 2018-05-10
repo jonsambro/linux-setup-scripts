aa803f7ea92bab4df551fa929127a84a08b6b3b8
Jonathan Brown <jonathan@jonathanbrown.ca>
2018-05-10 16:47:05 -0400

    Added configuration for vim

--- install_packages.sh
+++ install_packages.sh
@@ -1,6 +1,12 @@
 apt-get update
 apt-get upgrade
 
+# install basics
+sudo apt-get install vim
+export VISUAL=vim
+export EDITOR="$VISUAL"
+
+
 # install i3
 apt-get install libxcb1-dev libxcb-keysyms1-dev libpango1.0-dev libxcb-util0-dev libxcb-icccm4-dev libyajl-dev libstartup-notification0-dev libxcb-randr0-dev libev-dev libxcb-cursor-dev libxcb-xinerama0-dev libxcb-xkb-dev libxkbcommon-dev libxkbcommon-x11-dev autoconf libxcb-xrm-dev
 
@@ -13,3 +19,4 @@ sudo apt-get install git
 git config --global user.email "jonsambro@gmail.com"
 git config --global user.name "Jonathan Brown"
 git config --global push.default matching
+git config --global core.editor "vim"
