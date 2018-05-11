618e4dba394bea8bb66fc3236191c416c81d7b1a
Jonathan Brown <jonathan@jonathanbrown.ca>
2018-05-10 21:15:01 -0400

    modularized scripts

---
+++ git-install.sh
@@ -0,0 +1,6 @@
+# install git
+sudo apt-get install git
+git config --global user.email "jonsambro@gmail.com"
+git config --global user.name "Jonathan Brown"
+git config --global push.default matching
+git config --global core.editor "vim"
--- install_packages.sh
+++ install_packages.sh
@@ -8,15 +8,7 @@ export EDITOR="$VISUAL"
 
 
 # install i3
-apt-get install libxcb1-dev libxcb-keysyms1-dev libpango1.0-dev libxcb-util0-dev libxcb-icccm4-dev libyajl-dev libstartup-notification0-dev libxcb-randr0-dev libev-dev libxcb-cursor-dev libxcb-xinerama0-dev libxcb-xkb-dev libxkbcommon-dev libxkbcommon-x11-dev autoconf libxcb-xrm-dev
-
-sudo add-apt-repository ppa:aguignard/ppa
-sudo apt-get update
-sudo apt-get install libxcb-xrm-dev
+sudo ./i3-install.sh
 
 # install git
-sudo apt-get install git
-git config --global user.email "jonsambro@gmail.com"
-git config --global user.name "Jonathan Brown"
-git config --global push.default matching
-git config --global core.editor "vim"
+sudo ./git-install.sh
